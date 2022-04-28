39ee7aafe6599f66bf2143920738637a55856e2d
huysaokim <tvhtranviethuy1@gmail.com>
2022-04-28 11:30:29 +0700

    deploy

--- front-end/online-quiz/vue.config.js
+++ front-end/online-quiz/vue.config.js
@@ -4,5 +4,7 @@ module.exports = {
       port: 8080,
       public: '0.0.0.0:8080'
   },
-  publicPath: "/"
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/online-quiz-vueapp/' // Thay tên repository của các bạn vào đây nhé
+    : '/'
 }
